28b59a6fa281142a5aef1b488857da1986075df7
Elena Avduevskaia <avdi200595@gmail.com>
2019-07-03 20:50:44 +0300

    homework_6.1.2

--- app/src/main/java/ru/netology/lists/ListViewActivity.java
+++ app/src/main/java/ru/netology/lists/ListViewActivity.java
@@ -1,7 +1,11 @@
 package ru.netology.lists;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.os.Handler;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -9,30 +13,71 @@ import android.widget.SimpleAdapter;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ListViewActivity extends AppCompatActivity {
+public class ListViewActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     private List<Map<String,String>> text;
     private ListView listView;
+    SharedPreferences sharPref;
+    private SharedPreferences.Editor editor;
+    private ArrayList<Integer> saveChange = new ArrayList<Integer>();
+    private String [] arrayList;
+    private SwipeRefreshLayout swiperefresh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        swiperefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swiperefresh.setOnRefreshListener(this);
+        preferencesChack(savedInstanceState);
 
-         listView = findViewById(R.id.list);
+    }
+    public void onRefresh(){
+    new Handler().postDelayed(new Runnable() {
+        @Override
+        public void run() {
+        preferencesChack(null);
+        swiperefresh.setRefreshing(false);
+        }
+    }, 100);
+    }
 
-        List<Map<String,String>> values = prepareContent();
-
-        BaseAdapter listContentAdapter = createAdapter(values);
-
+    private void preferencesChack(Bundle bundle){
+        sharPref = getSharedPreferences("Satting", Context.MODE_PRIVATE);
+        editor = sharPref.edit();
+        if(sharPref.contains("TEXT")){
+        arrayList = sharPref.getString("TEXT", "Текст отсутствует").split("\n\n");
+        } else {
+            editor.putString("TEXT", getResources().getString(R.string.large_text));
+            editor.apply();
+            arrayList = sharPref.getString("TEXT", "Текст отсутствует").split("\n\n");
+        }
+        listView = findViewById(R.id.list);
+        final List<Map<String,String>> values = prepareContent();
+        final BaseAdapter listContentAdapter = createAdapter(values);
         listView.setAdapter(listContentAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                saveChange.add(position);
+                values.remove(position);
+                listContentAdapter.notifyDataSetChanged();
+            }
+        });
+        if(bundle != null) {
+            saveChange.addAll(bundle.getIntegerArrayList("saveList"));
+            for (int index : saveChange) {
+                values.remove(index);
+            }
+        }
     }
 
     @NonNull
@@ -44,7 +89,6 @@ public class ListViewActivity extends AppCompatActivity {
     @NonNull
     private List<Map<String,String>> prepareContent() {
        text= new ArrayList<>();
-        String[] arrayList = getString(R.string.large_text).split("\n\n");
         for (int i = 0; i < arrayList.length; i++) {
         Map<String,String> mapString = new HashMap<>();
         mapString.put("nameText", arrayList[i]);
@@ -53,4 +97,10 @@ public class ListViewActivity extends AppCompatActivity {
         }
         return text;
     }
+
+    protected void onSaveInstanceState(Bundle outState){
+        outState.putIntegerArrayList("saveList", saveChange);
+        super.onSaveInstanceState(outState);
+    }
+
 }
